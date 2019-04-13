5ee223bcc43dde4ef81218dd6ec09bc8a73176a9
evanchau0912 <s3634247@rmit.edu.vn>
2019-04-13 14:03:38 +0700

    try to commit

---
+++ Pods/Target Support Files/Pods-AwesomePhoto/Pods-AwesomePhoto-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_AwesomePhoto : NSObject
+@end
+@implementation PodsDummy_Pods_AwesomePhoto
+@end
---
+++ Pods/Target Support Files/Pods-AwesomePhotoUITests/Pods-AwesomePhotoUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_AwesomePhotoUITests : NSObject
+@end
+@implementation PodsDummy_Pods_AwesomePhotoUITests
+@end
