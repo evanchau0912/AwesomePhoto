af764cf023290db582adc5af322ff7c959196dfc
Tuong Chau <s3634247@rmit.edu.vn>
2019-04-13 14:55:33 +0700

    Add tutorial screen

---
+++ AwesomePhoto/Tutorial Screen/TutorialViewController.swift
@@ -0,0 +1,30 @@
+//
+//  TutorialViewController.swift
+//  AwesomePhoto
+//
+//  Created by Chau Phuoc Tuong on 4/13/19.
+//  Copyright © 2019 Chau Phuoc Tuong. All rights reserved.
+//
+
+import UIKit
+
+class TutorialViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
